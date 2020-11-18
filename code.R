efac002dc4fee4b56262f337932ed7f9d1befec4
Ella Lamont <elamont@uw.edu>
2020-11-18 11:13:11 -0800

    committing new file to github

---
+++ code.R
@@ -0,0 +1,9 @@
+# Code # 
+
+library(tidyverse)
+
+# Git actions
+# pull: pull changes from remote repo (if you are in collaboration with others)
+# add: stage files for commit; click on stage box under git pane
+# commit: commit to the local repo; click on "commit" button under git pane; add a commit message
+# push: push to the remote repo on Github
